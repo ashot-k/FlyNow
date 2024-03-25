358b0755f628fd4917e6e2354200bfd0d4458a4e
ashot-k <karamanidist@gmail.com>
2024-03-25 18:28:42 +0200

    Initial commit

---
+++ src/test/java/org/flynow/FlyNowApplicationTests.java
@@ -0,0 +1,10 @@
+package org.flynow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlyNowApplicationTests {
+
+    @Test
+    void contextLoads() {
