bb8e2bfb3845ca55f07895249609e79eb73e15c4
ashot <karamanidist@gmail.com>
2024-05-03 22:10:07 +0300

    entities

--- src/main/java/org/flynow/entity/Flight.java
+++ src/main/java/org/flynow/entity/Flight.java
@@ -11,13 +11,12 @@ public class Flight {
     @Column(name = "id", nullable = false)
     private Long id;
 
-    private String flightNo;
     private String origin;
     private String destination;
     private int stops;
     private LocalDate departureDate;
     private LocalDate returnDate;
-    private String flightType;
+    private Long price;
 
 
     public Long getId() {
@@ -26,5 +25,53 @@ public class Flight {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public int getStops() {
+        return stops;
+    }
+
+    public void setStops(int stops) {
+        this.stops = stops;
+    }
+
+    public LocalDate getDepartureDate() {
+        return departureDate;
+    }
+
+    public void setDepartureDate(LocalDate departureDate) {
+        this.departureDate = departureDate;
+    }
+
+    public LocalDate getReturnDate() {
+        return returnDate;
+    }
+
+    public void setReturnDate(LocalDate returnDate) {
+        this.returnDate = returnDate;
+    }
+
+    public Long getPrice() {
+        return price;
+    }
+
+    public void setPrice(Long price) {
+        this.price = price;
     }
 }
